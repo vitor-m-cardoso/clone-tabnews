c58ad1e8a2037afa6970bd4a7a78bffcc463d8d0
Vitor Cardoso <vitor.mauricioc@outlook.com>
2024-06-10 16:17:42 -0300

    adiciona `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-const status = (req, res) => {
-  res.status(200).json({ message: 'Deu tudo certo!' });
+import database from '../../../../infra/database.js';
+
+async function status(_req, res) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  return res.status(200).json({ message: 'Deu tudo certo!' });
 };
 
 export default status;
