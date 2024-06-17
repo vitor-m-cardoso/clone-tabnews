c76ca82bf204f2b2afc7694fd605a98eb8739be5
Vitor Cardoso <vitor.mauricioc@outlook.com>
2024-06-17 11:34:03 -0300

    altera caminho relativo para absoluto

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,8 @@
-import database from '../../../../infra/database.js';
+import database from 'infra/database.js';
 
 async function status(_req, res) {
   const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log(result.rows);
   return res.status(200).json({ message: 'Deu tudo certo!' });
 };
 
